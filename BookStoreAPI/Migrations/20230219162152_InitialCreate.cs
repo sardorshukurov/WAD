917092c2608584fce5c2953d3eb912a9b4ead777
Sardor Shukurov <sshukurov@students.wiut.uz>
2023-02-21 13:49:08 +0500

    Moved Back-end to separate folder

---
+++ BookStoreAPI/Migrations/20230219162152_InitialCreate.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WAD.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Biography = table.Column<string>(maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(maxLength: 100, nullable: false),
+                    AuthorId = table.Column<int>(nullable: false),
+                    Publisher = table.Column<string>(maxLength: 100, nullable: false),
+                    PublicationDate = table.Column<DateTime>(nullable: false),
+                    ISBN = table.Column<string>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    Quantity = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false),
+                    Image = table.Column<string>(nullable: true),
+                    Rating = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Books_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_CategoryId",
+                table: "Books",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ BookStoreAPI/Models/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace WAD.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a title")]
+        [MinLength(5), MaxLength(100)]
+        public string Title { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        [Required(ErrorMessage = "Please enter a publisher")]
+        [MinLength(5), MaxLength(100)]
+        public string Publisher { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Publication Date")]
+        public DateTime PublicationDate { get; set; }
+
+        [Required(ErrorMessage = "Please enter an ISBN")]
+        [RegularExpression(@"^(97(8|9))?\d{9}(\d|X)$", ErrorMessage = "Please enter a valid ISBN")]
+        public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "Please enter a price")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please enter a quantity")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter a positive quantity")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description")]
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public string Image { get; set; }
+
+        [Range(0, 5, ErrorMessage = "Please enter a rating between 0 and 5")]
+        public double Rating { get; set; }
+    }
+}
